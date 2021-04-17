b36bd367e808fa3c2528d8511d1dab678a453c89
Kpatain <62848924+Kpatain@users.noreply.github.com>
2021-04-17 18:10:30 +0200

    init

---
+++ huffman/huffman/Source1.cpp
@@ -0,0 +1,15 @@
+/*
+
+3) Il s'agit d'un arbre dont chacune des branches crée 2 branches (0 ou 1).
+Dans le cas de l'arbre de Huffman, les deux choix sont composés d'une branche ou d'une feuille.
+
+5) En ascii, "Coucou" aurait pris 6*7 = 42 bits. Ici il en prend 12.
+
+6) C'est la taille des charactère en binaire. En ascii, il sont tous composé de 7 bits. 
+Ici leurs tailles varies, on ne peut savoir quand un charactère se termine et un autre commence.
+
+
+
+
+
+*/

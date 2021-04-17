d1f3143944298a7daa138cdca541c3c438c2eddc
Kpatain <62848924+Kpatain@users.noreply.github.com>
2021-04-17 19:34:51 +0200

    counting

--- Huffman_CALLEDE/Huffman_CALLEDE/Source.cpp
+++ Huffman_CALLEDE/Huffman_CALLEDE/Source.cpp
@@ -1,16 +1,44 @@
 #include <iostream>
 #include <fstream>
+#include <map>
 
 using namespace std;
 
 int main()
 {
+	//DECLARE
 	fstream fs;
-	fs.open("Lyrics.txt");
+	map<char, int> charMap;
 	char c;
+	
+	//OPENING TXT
+	fs.open("Lyrics.txt");
+	
+	//PLAYING LOOP
 	while (fs.get(c))
 	{
+		
+		//PRINTING TXT
 		cout << c;
+
+		//MAP IMPLEMENTATION
+		map<char, int>::iterator it = charMap.find(c);
+		//IL EXISTE
+		if (it == charMap.end())
+		{
+			charMap.insert({ c,1 });
+		}
+
+		//IL EXISTE PAS ENCORE
+		else
+		{
+			it->second += 1;
+		}
+	}
+
+	//PRINTING THE MAP ZEBI
+	for (const auto& p : charMap) {
+		std::cout << "Char [" << p.first << "] = " << p.second << " iterations." << endl;
 	}
 
 	return 0;
